c1354477f0ea77646121c3013510107b2f57e59f
sairutdinov <r.sairutdinov@haulmont.com>
2021-10-12 10:17:46 +0400

    Initial commit

---
+++ src/test/java/com/course/msacourse/MsaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.msacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
